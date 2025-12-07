f52d2e5b9e44dd95d35dc8daa48f93aaf6dd2b88
Deep Anilkumar Prajapati <prajapati.dee@northeastern.edu>
2025-12-07 16:48:35 -0500

    final fixes and done

--- COMMUNITY ISSUE REPORTING & RESOLUTION MANAGEMENT SYSTEM_Group10/src/ui/common/ViewNotificationsJPanel.java
+++ COMMUNITY ISSUE REPORTING & RESOLUTION MANAGEMENT SYSTEM_Group10/src/ui/common/ViewNotificationsJPanel.java
@@ -28,7 +28,7 @@ public class ViewNotificationsJPanel extends javax.swing.JPanel {
         setupTable();
         populateNotifications();
     }
-
+    //created notifications feature in this
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
